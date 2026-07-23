030e925520cea4a7eb1d6673e70b5d1f55a2bfbd
Frank Kalengay <frankkalengay@gmail.com>
2026-07-23 09:28:37 +0200

    feat: WIP service

---
+++ account-service/src/main/java/za/ac/banking/service/AccountEventConsumer.java
@@ -0,0 +1,4 @@
+package za.ac.banking.service;
+
+public class AccountEventConsumer {
+}
---
+++ account-service/src/main/java/za/ac/banking/service/AccountService.java
@@ -0,0 +1,7 @@
+package za.ac.banking.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountService {
+}
