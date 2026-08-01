ea54e52f3425882c45ff1ed709767de125f36f9d
Frank Kalengay <frankkalengay@gmail.com>
2026-08-01 23:59:36 +0200

    feat: update account response dto

--- account-service/src/main/java/za/ac/banking/dto/AccountResponse.java
+++ account-service/src/main/java/za/ac/banking/dto/AccountResponse.java
@@ -1,11 +1,17 @@
 package za.ac.banking.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import za.ac.banking.model.AccountStatus;
 import za.ac.banking.model.AccountType;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AccountResponse {
 
     private String id;
