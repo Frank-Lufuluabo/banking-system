bb4fc03a50ab583b53700a07620d248795d445a3
Frank Kalengay <frankkalengay@gmail.com>
2026-08-02 23:55:53 +0200

    feat: WIP account repository

--- account-service/src/main/java/za/ac/banking/repository/AccountRepository.java
+++ account-service/src/main/java/za/ac/banking/repository/AccountRepository.java
@@ -4,4 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import za.ac.banking.model.Account;
 
 public interface AccountRepository extends JpaRepository<Account,String> {
+
+    boolean existsByEmail(String email);
+    boolean existsByAccountNumber(String accountNumber);
 }
