7ab09fe6a22e4c40790827b246e89a4726854aeb
Frank Kalengay <frankkalengay@gmail.com>
2026-08-05 23:26:42 +0200

    feat: add findByAccountNumber method

--- account-service/src/main/java/za/ac/banking/repository/AccountRepository.java
+++ account-service/src/main/java/za/ac/banking/repository/AccountRepository.java
@@ -3,8 +3,11 @@ package za.ac.banking.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import za.ac.banking.model.Account;
 
+import java.util.Optional;
+
 public interface AccountRepository extends JpaRepository<Account,String> {
 
     boolean existsByEmail(String email);
     boolean existsByAccountNumber(String accountNumber);
+    Optional<Account> findByAccountNumber(String accountNumber);
 }
